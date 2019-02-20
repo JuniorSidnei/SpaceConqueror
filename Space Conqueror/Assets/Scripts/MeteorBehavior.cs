d7161aa11e7ef82b4e056891c6ab9bca71c58233
Sidnei <sidneicarrarojunior@gmail.com>
2019-02-19 22:22:07 -0300

    meteoros sendo criados, colcoar rotação pra dar um gamefeel e uma particula de poeira algo assim

---
+++ Space Conqueror/Assets/Scripts/MeteorBehavior.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteorBehavior : MonoBehaviour
+{
+    //Posição e limite X de spawn
+    public float _spawnX;
+    //Posição e limite Y de spawn
+    public float _spawnY;
+    //Tempo de spawn
+    private float _spawnTimer = 1f;
+    //Objeto do meteoro
+    public GameObject _meteor;
+    
+
+    
+    void Update()
+    {
+        
+
+        //Tempo de spawn de um meteoro pra outro que tbm vai somar com o tempo
+        _spawnTimer -= Time.deltaTime;
+
+        if(_spawnTimer <= 0)
+        {
+            SpawnMeteor();
+            _spawnTimer = 1f;
+        }
+
+    }
+
+    //Função de spawn do meteoro
+    void SpawnMeteor()
+    {
+        
+        //Instanciando o meteoro
+        GameObject tempoMeteor = Instantiate(_meteor, new Vector2(_spawnX, Random.Range(-_spawnY, _spawnY)), Quaternion.identity, transform);
+        //Destruindo o meteoro
+        Destroy(tempoMeteor, 5f);
+    }
+
+}
---
+++ Space Conqueror/Assets/Scripts/MeteorStatus.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeteorStatus : MonoBehaviour
+{
+    //Velocidade do meteoro
+    public float _meteorSpeed;
+    //Tempo do jogo em segundos pra somar coma  velocidade do meteoro
+    private float _timer;
+    //Vida do meteoro, que vai ser um random de 50 e 80
+    public int _meteorLife;
+    //Vetor temporário para a escala do meteoro
+    private Vector3 _tempScale;
+    //Referencia ao rigidbody
+    private Rigidbody2D _meteorRb;
+   
+    void Start()
+    {
+        //Pegando rigidbody
+        _meteorRb = GetComponent<Rigidbody2D>();
+        //Vida dos meteoros quando nascem
+        _meteorLife = Random.Range(50, 80);
+        //Atribuindo o valor da escala ao vetor temporario
+        _tempScale = transform.localScale;
+        //Atribuindo uma escala aleátoria para o meteoro
+        _tempScale.x = Random.Range(0.4f, 1f);
+        _tempScale.y = Random.Range(0.4f, 1f);
+        //Retornando o valor alterado para a escala do objeto
+        transform.localScale = _tempScale;
+
+        
+    }
+
+    
+    void Update()
+    {
+        //Tempo do jogo em segundos
+        _timer += Time.deltaTime;
+        
+
+        //Movendo o meteoro 
+        _meteorRb.MovePosition(transform.position + transform.right * -_meteorSpeed * Time.deltaTime * _timer/2);
+    }
+}
