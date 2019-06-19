1631d3e0e8d8bdb426384b9b8be2256d5701bbe0
Sidnei <sidneicarrarojunior@gmail.com>
2019-06-19 18:24:04 -0300

    Velocidade com feedback na hud

--- Space Conqueror/Assets/Scripts/Hud/PanelControllerPlaying.cs
+++ Space Conqueror/Assets/Scripts/Hud/PanelControllerPlaying.cs
@@ -29,32 +29,28 @@ public class PanelControllerPlaying : BaseHudBehavior
     private const float PLAYNG_POS_Y = -120f;
     private const float CONVERSATION_POS_Y = 540f;
 
+    private void Start()
+    {
+        m_controlPlayer = FindObjectOfType<ControlPlayer>();
+    }
+
     private void Update()
     {
         m_playerLifeText.text = ("" + m_playerInfo.CurrentLife);
-        m_speedometerFill.DOFillAmount(m_playerInfo.Speed / 1000, 1f);
-    }
-
-    //Informações do jogador
-    public override void SetPlayerInfo(PlayerInfo playerInfo)
-    {
-        m_playerInfo = playerInfo;
-        playerInfo.OnValueChanged += UpdateHudValues;
-    }
-
-
-    private void UpdateHudValues(PlayerInfo playerinfo, ControlPlayer controlPlayer)
-    {
-        if (playerinfo == null || controlPlayer == null) return; 
+        m_LifeBarFill.DOFillAmount((float)m_playerInfo.CurrentLife / m_playerInfo.MaxLife, 2f);
         
-        m_LifeBarFill.DOFillAmount((float)playerinfo.CurrentLife / playerinfo.MaxLife, 2f);
-        //m_playerLifeText.text = ("" + playerinfo.CurrentLife);
-
-        if (playerinfo.RecoveryKit <= 0)
+        m_speedometerFill.DOFillAmount(m_controlPlayer._moveVelocity.sqrMagnitude / 100, 1f);
+        
+        if (m_playerInfo.RecoveryKit <= 0)
             m_recoveryKitKey.gameObject.GetComponent<Image>().DOColor(Color.black, 1f);
     }
 
-
+    //Informações iniciais do jogador
+    public override void SetPlayerInfo(PlayerInfo playerInfo)
+    {
+        m_playerInfo = playerInfo;
+    }
+    
     public override void HandleConversation()
     {
         base.HandleConversation();
@@ -67,7 +63,7 @@ public class PanelControllerPlaying : BaseHudBehavior
     {
         base.HandlePlaying();
         m_lifeBox.gameObject.transform.DOScale(new Vector3(1, 1,  0),1f);
-        m_recoveryBox.gameObject.transform.DOScale(new Vector3(1, 1, 0), 1.2f);
-        m_speedometerBox.gameObject.transform.DOScale(new Vector3(1, 1, 1), 1.5f);
+        m_recoveryBox.gameObject.transform.DOScale(new Vector3(1, 1, 0), 2f);
+        m_speedometerBox.gameObject.transform.DOScale(new Vector3(1, 1, 1), 3f);
     }
 }
